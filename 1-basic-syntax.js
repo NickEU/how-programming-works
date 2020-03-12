9b27552a7d225e7b6384b8f8572fef26c0a6b3c6
Nick <neverate@gmail.com>
2020-03-12 21:55:55 +0200

    + identifiers/loops

--- 1-basic-syntax.js
+++ 1-basic-syntax.js
@@ -1,1 +1,34 @@
 'use strict';
+
+//1
+const name = 'Nick';
+//2
+const YOB = 1985;
+
+console.log(`${YOB} was a very good year`);
+//3
+const greet = name => {
+  console.log(`Hello there, ${name}!`);
+};
+
+greet(name);
+
+//4
+
+function range(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+}
+
+console.log(range(15, 30));
+
+//5
+
+function rangeOdd(start, end) {
+  const result = [];
+  for (let i = start; i <= end; i += 2) {
+    result.push(i);
+  }
+  return result;
+}
+
+console.log(rangeOdd(15, 30));
